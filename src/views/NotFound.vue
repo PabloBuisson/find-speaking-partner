5b1d3db6d3165c9a62028e42769d189e905d4771
Pablo Buisson <pablo.buisson@gmail.com>
2022-10-14 07:35:22 +0200

    feat: add content to the NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Oops, page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/partners">partners</router-link> ?
+      </p>
+    </base-card>
+  </section>
+</template>
